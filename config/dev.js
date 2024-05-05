46b07d0bdec69e98937731416382083097c872fc
danielstet <stetsyukdaniel@gmail.com>
2024-05-05 14:28:40 +0300

    finished auth flow

---
+++ config/dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+    googleClientID : '96013173848-2k24081dhvub6t9af0l78638bc205ho9.apps.googleusercontent.com',
+    googleClientSecret : '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',
+    mongoURI: 'mongodb+srv://danielstet:Dae3w2q1@cluster0.gb6mq1y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    cookieKey: 'hdfsakljhfkjlhfjkHSKJH123213456879hjkl'
+};
+
+// ClientID: 518567654403-srsm3nnvgssdl86ceg4bc18g96j1u49h.apps.googleusercontent.com
+// ClientSecret: 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
