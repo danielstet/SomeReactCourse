32df9f5cecfe0feb31828c31af51090a410a8b47
danielstet <stetsyukdaniel@gmail.com>
2024-05-07 13:10:34 +0300

    tweak google strategy settings

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,8 @@ module.exports = {
     googleClientID : '96013173848-2k24081dhvub6t9af0l78638bc205ho9.apps.googleusercontent.com',
     googleClientSecret : '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',
     mongoURI: 'mongodb+srv://danielstet:Dae3w2q1@cluster0.gb6mq1y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-    cookieKey: 'hdfsakljhfkjlhfjkHSKJH123213456879hjkl'
+    cookieKey: 'hdfsakljhfkjlhfjkHSKJH123213456879hjkl',
+    googleRedirectURI: 'https://bayou.herokuapp.com',
+    proxy: true
 };
 
